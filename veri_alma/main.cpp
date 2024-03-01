34e270b6fe4542ae805a73ce9ea89166823a52c1
yemce <yunusemreclk0hfl@gmail.com>
2024-03-02 01:44:07 +0300

    kullanıcıdan veri alma

---
+++ veri_alma/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  string isim;
+  int yas;
+  cout << "Adını yaz: ";
+  cin >> isim;
+  cout << "Yaşını yaz: ";
+  cin >> yas;
+  cout << "Merhaba " << isim;
+  cout << " yaşın " << yas;
+}
